8cda831e1f9a66a58f72b663b9bd82392270b5f7
tmknom <tmk.nom@gmail.com>
2018-10-21 21:51:38 +0900

    Add --no-filename option

--- terraform.mk
+++ terraform.mk
@@ -1,7 +1,7 @@
 .DEFAULT_GOAL := help
 
 # https://gist.github.com/tadashi-aikawa/da73d277a3c1ec6767ed48d1335900f3
-.PHONY: $(shell grep -E '^[a-zA-Z_-]+:' $(MAKEFILE_LIST) | sed 's/://')
+.PHONY: $(shell grep --no-filename -E '^[a-zA-Z_-]+:' $(MAKEFILE_LIST) | sed 's/://')
 
 # Constant definitions
 TERRAFORM_IMAGE := hashicorp/terraform:${TERRAFORM_VERSION}
@@ -104,4 +104,4 @@ docs: ## Generate docs
 
 # https://postd.cc/auto-documented-makefile/
 help: ## Show help
-	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
+	@grep --no-filename -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
