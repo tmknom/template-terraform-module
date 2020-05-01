3c30d8d15898c9e6246b8c0b274c9960006ca9d1
tmknom <tmk.nom@gmail.com>
2020-05-01 15:06:19 +0900

    Upgraded examples/complete to support Terraform 0.12

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,9 +1,9 @@
 output "null_resource_id" {
-  value       = "${module.complete.null_resource_id}"
+  value       = module.complete.null_resource_id
   description = "An arbitrary value that changes each time the resource is replaced."
 }
 
 output "example_value" {
-  value       = "${module.complete.example_value}"
+  value       = module.complete.example_value
   description = "Example variable."
 }
